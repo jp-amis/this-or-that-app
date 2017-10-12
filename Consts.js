0777a2368f3cdfebe6bccff7e591dc3de13b6185
jp <juaumlol@gmail.com>
2017-10-12 18:58:07 -0300

    back to production

--- Consts.js
+++ Consts.js
@@ -1,9 +1,9 @@
 module.exports = {
-    API_URL: 'http://localhost:3000',
-    IMAGE_URL: 'http://localhost:3000/uploads/',
+    API_URL: 'http://thisorthat.us-east-1.elasticbeanstalk.com',
+    IMAGE_URL: 'http://thisorthat.us-east-1.elasticbeanstalk.com/uploads/',
     TOKEN_KEY: 'token',
     QUANTITY_CARDS_TO_LOAD: 100,
 };
 
-// API_URL: 'http://thisorthat.us-east-1.elasticbeanstalk.com',
-// IMAGE_URL: 'http://thisorthat.us-east-1.elasticbeanstalk.com/uploads/',
+// API_URL: 'http://localhost:3000',
+// IMAGE_URL: 'http://localhost:3000/uploads/',
